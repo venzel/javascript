5f5e3a7bff77fdb66533f3b4ec3f6caf31ebfa7e
eeneas <eeneas@indracompany.com>
2022-03-09 19:00:48 -0300

    feat: setup

---
+++ codes/filterFieldsObjectArray.js
@@ -0,0 +1,36 @@
+
+const filterFieldsObject = (objctToFilter = {}, arrayFields = []) => {
+    const contains = (target, pattern) => {
+        let value = 0;
+
+        pattern.forEach(word => {
+            value = value + target.includes(word);
+        });
+        
+        return (value === 1);
+    }
+
+    const filterFields = (obj) => Object.keys(obj)
+            .filter((key) => contains(key, arrayFields))
+            .reduce((current, key) => Object.assign(current, { [key]: obj[key] }), {});
+
+    return objctToFilter.map(e => filterFields(e));
+}
+
+const object = [
+    { name: 'Tiago', admin: 'Vericard', age: 59 },
+    { name: 'Betania', admin: 'Mainard', age: 57 },
+    { name: 'Vanessa', admin: 'Venzel', age: 38 }
+]
+
+const fields = ['name', 'age'];
+
+const objectFiltered = filterFieldsObject(object,fields);
+
+console.log(objectFiltered);
+
+// [
+//    { name: 'Tiago', age: 59 },
+//    { name: 'Betania', age: 57 },
+//    { name: 'Vanessa', age: 38 }
+// ]
---
+++ codes/intersectionOfSets.js
@@ -0,0 +1,25 @@
+const field_a = '2 7 4'
+const field_b = '5 3'
+const field_c = '4 1'
+
+const a = [...field_a.split(' ')]
+const b = [...field_b.split(' ')]
+const c = [...field_c.split(' ')]
+
+// Intercection
+const int_ab = [...a].filter(e => b.includes(e));
+const int_ac = [...a].filter(e => c.includes(e));
+const int_bc = [...b].filter(e => c.includes(e));
+
+const array = [...int_ab, ...int_ac, ...int_bc]
+
+console.log(array);
+
+if (array.length) {
+    console.log('Repete!');
+} else {
+    console.log('Nao repete!');
+}
+
+// [ '4' ]
+// Repete!
