fcddd56d91540867bbe9a5aeaae6aa0ca9c96e09
venzel <eneas.eng@yahoo.com>
2022-03-20 15:39:10 -0300

    feat: using map and reduce to calculate total

---
+++ codes/mapAndReduceToSumFinal.js
@@ -0,0 +1,16 @@
+const gamers = [
+    { name: 'A', metrics: 8.0 },
+    { name: 'B', metrics: 8.0 },
+    { name: 'C', metrics: 8.0 },
+    { name: 'D', metrics: 8.0 },
+];
+
+const result = gamers
+    .map((g) => g.metrics)
+    .reduce((accumulator, current) => {
+        return accumulator + current;
+    }, 0);
+
+console.log(result);
+
+// 32
