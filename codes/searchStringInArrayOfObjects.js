3004d8ff335ed1c955cfe45300e3efd1d4bcf189
venzel <eneas.eng@yahoo.com>
2022-06-05 13:32:15 -0300

    feat: busca string em array de objetos

---
+++ codes/searchStringInArrayOfObjects.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        id: 10,
+        name: 'Tiago',
+    },
+    {
+        id: 20,
+        name: 'Marta',
+    },
+    {
+        id: 30,
+        name: 'Leticia',
+    },
+    {
+        id: 40,
+        name: 'Jose',
+    },
+];
+
+const search = (name) => {
+    return users.filter((user) => {
+        return user.name.toLowerCase().trim().search(name.toLowerCase().trim()) !== -1;
+    });
+};
+
+console.log(search('ti'));
